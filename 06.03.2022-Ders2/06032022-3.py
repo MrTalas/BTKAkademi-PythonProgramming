e93e74815e0a4f13cc6bdca189df9a28212b1db2
MrTalas <90217438+MrTalas@users.noreply.github.com>
2022-03-06 11:50:43 +0300

    Add files via upload

---
+++ 06.03.2022-Ders2/06032022-3.py
@@ -0,0 +1,8 @@
+gunler=["Pazartesi","Çarşamba","Cuma"]
+print(gunler)
+gunler.pop() #Listenin son elemanını siler. Parantz içine yazılan indexe göre de silme işlemi yapabilir.
+print(gunler)
+gunler=["Pazartesi","Çarşamba","Cuma"]
+print(gunler)
+gunler.pop(0) #0. indexi sildik.
+print(gunler)
